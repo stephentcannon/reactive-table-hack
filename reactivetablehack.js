8da36dd71dd4dc1af65f9a551f6ad870206acff9
system_user <stephentcannon@gmail.com>
2014-08-27 20:56:24 -0400

    added search box

--- reactivetablehack.js
+++ reactivetablehack.js
@@ -1,4 +1,15 @@
 ReactiveTableHack = {
+  injectKeywordFilter: function(template){
+    var reactiveTableFilter = template.find('.reactive-table-filter');
+    //var reactiveTableFiler = $('.reactive-table-filter');
+    var html;
+    html = '<span class="input-group-addon"><i class="fa fa-search"></i></span><input class="form-control" type="text" id="keywords" name="keywords" value="' + Session.get('keywords') + '" data-toggle="tooltip" title="Enter keywords">';
+    reactiveTableFilter.childNodes.item(1).className = 'input-group col-sm-10 col-sm-offset-2';
+    var inputGroup = reactiveTableFilter.childNodes.item(1);
+    inputGroup.innerHTML = html + inputGroup.innerHTML;
+  
+    Session.set('waivers-reactive-table-filter', null);
+  },
   injectIntoTableFilter: function(template){
     var reactiveTableFilter = template.find('.reactive-table-filter');
     //var reactiveTableFiler = $('.reactive-table-filter');
@@ -21,7 +32,7 @@ ReactiveTableHack = {
     // console.log( event.target.value);
     // console.log('event.currentTarget');
     // console.log(event.currentTarget);
-    if(event.target.name == 'start_date' || event.target.name == 'end_date'){
+    if(event.target.name == 'start_date' || event.target.name == 'end_date' || event.target.name == 'keywords'){
      //  console.log('start or end date');
       event.stopPropagation();
       event.stopImmediatePropagation();
@@ -29,6 +40,13 @@ ReactiveTableHack = {
     } else {
       // console.log('this is not the start or end date');
     }
+  },
+  blurKeywordEvent: function(event, template, name){
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    // console.log('blur #end_date');
+    Session.set(name, event.target.value);
   },
   blurDateEvent: function(event, template, name){
     // console.log('blurDateEvent');
